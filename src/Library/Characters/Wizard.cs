3a2cebfa57ffd47ce77f17971e7af5b7aacc3ae2
Dana Castroman <danacastroman@gmail.com>
2021-10-07 18:57:12 -0300

    clase abstracta

--- src/Library/Characters/Wizard.cs
+++ src/Library/Characters/Wizard.cs
@@ -1,35 +1,24 @@
 using System.Collections.Generic;
 namespace RoleplayGame
 {
-    public class Wizard: IMagicCharacter
+    public class Wizard: Character, IMagicCharacter
     {
-        private int health = 100;
-
-        private List<IItem> items = new List<IItem>();
-
         private List<IMagicalItem> magicalItems = new List<IMagicalItem>();
 
         public Wizard(string name)
+            : base(name)
         {
             this.Name = name;
             
             this.AddItem(new Staff());
         }
-
-        public string Name { get; set; }
         
-        public int AttackValue
+        public override int AttackValue
         {
             get
             {
                 int value = 0;
-                foreach (IItem item in this.items)
-                {
-                    if (item is IAttackItem)
-                    {
-                        value += (item as IAttackItem).AttackValue;
-                    }
-                }
+                value += base.AttackValue;
                 foreach (IMagicalItem item in this.magicalItems)
                 {
                     if (item is IMagicalAttackItem)
@@ -41,18 +30,12 @@ namespace RoleplayGame
             }
         }
 
-        public int DefenseValue
+        public override int DefenseValue
         {
             get
             {
                 int value = 0;
-                foreach (IItem item in this.items)
-                {
-                    if (item is IDefenseItem)
-                    {
-                        value += (item as IDefenseItem).DefenseValue;
-                    }
-                }
+                value += base.DefenseValue;
                 foreach (IMagicalItem item in this.magicalItems)
                 {
                     if (item is IMagicalDefenseItem)
@@ -62,41 +45,6 @@ namespace RoleplayGame
                 }
                 return value;
             }
-        }
-
-        public int Health
-        {
-            get
-            {
-                return this.health;
-            }
-            private set
-            {
-                this.health = value < 0 ? 0 : value;
-            }
-        }
-
-        public void ReceiveAttack(int power)
-        {
-            if (this.DefenseValue < power)
-            {
-                this.Health -= power - this.DefenseValue;
-            }
-        }
-
-        public void Cure()
-        {
-            this.Health = 100;
-        }
-
-        public void AddItem(IItem item)
-        {
-            this.items.Add(item);
-        }
-
-        public void RemoveItem(IItem item)
-        {
-            this.items.Remove(item);
         }
 
         public void AddItem(IMagicalItem item)
